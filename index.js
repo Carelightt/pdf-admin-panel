0309cc21f884423e7bd6f4d878bcfb424280f441
Carelightt <1carelight@gmail.com>
2025-05-24 20:08:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ app.post('/admin-login', (req, res) => {
 
 app.post('/generate', async (req, res) => {
   const { tc, ad, soyad } = req.body;
-  const templatePath = path.join(__dirname, 'public', 'sablon.pdf');
+  const selectedTemplate = req.body.template === 'sablon2' ? 'sablon2.pdf' : 'sablon.pdf';
+const templatePath = path.join(__dirname, 'public', selectedTemplate);
   const fontPath = path.join(__dirname, 'fonts', 'LiberationSans-Bold.ttf');
 
   const existingPdfBytes = fs.readFileSync(templatePath);
